df32750dd6b6bccaa0441b93de51d7da2eca7e18
SergDerbst <sergio.weigel@gmail.com>
2021-07-15 09:43:31 +0200

    just commit

---
+++ src/app/_components/content/editable/image/editable.image.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'tmt-editable-image',
+  templateUrl: 'editable.image.component.html',
+  styleUrls: ['editable.image.component.scss']
+})
+export class EditableImageComponent {
+
+}
---
+++ src/app/_components/content/editable/image/editable.image.module.ts
@@ -0,0 +1,10 @@
+import {NgModule} from "@angular/core";
+import {EditableImageComponent} from "./editable.image.component";
+import {CommonModule} from "@angular/common";
+
+@NgModule({
+  declarations: [ EditableImageComponent ],
+  imports: [ CommonModule ],
+  exports: [ EditableImageComponent ]
+})
+export class EditableImageModule {}
